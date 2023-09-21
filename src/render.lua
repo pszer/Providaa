4fc2958bba86e63fd38057eb5f283f9bccea1213
pszer <pszerszynski256@gmail.com>
2023-09-21 07:09:39 +0100

    commit

--- src/render.lua
+++ src/render.lua
@@ -40,7 +40,7 @@ Renderer = {
 
 	enable_hdr = true,
 	hdr_exposure = 0.15,
-	hdr_exposure_min = 0.01,
+	hdr_exposure_min = 0.1,
 	hdr_exposure_max = 2.0,
 	hdr_exposure_nudge = 1.15,
 	hdr_exposure_adjust_speed = 2.5,
